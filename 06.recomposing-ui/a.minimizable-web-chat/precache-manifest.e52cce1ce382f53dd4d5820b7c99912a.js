ced130e347015ea8ee63126de2e9286672c73402
William Wong <compulim@hotmail.com>
2020-03-02 11:11:25 +0000

    Based on commit e423f607ccf523ca6b81793d32e719717c1d4fd1

---
+++ 06.recomposing-ui/a.minimizable-web-chat/precache-manifest.e52cce1ce382f53dd4d5820b7c99912a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5546f3c1a16925741d9bd3e8e656ca",
+    "url": "/BotFramework-WebChat/06.recomposing-ui/a.minimizable-web-chat/index.html"
+  },
+  {
+    "revision": "4f7f879573c5e491049d",
+    "url": "/BotFramework-WebChat/06.recomposing-ui/a.minimizable-web-chat/static/css/main.13626e63.chunk.css"
+  },
+  {
+    "revision": "f4e394a23637d305b9bf",
+    "url": "/BotFramework-WebChat/06.recomposing-ui/a.minimizable-web-chat/static/js/2.7abfeff6.chunk.js"
+  },
+  {
+    "revision": "ffb96c4a209f458eda301ec9e19e1551",
+    "url": "/BotFramework-WebChat/06.recomposing-ui/a.minimizable-web-chat/static/js/2.7abfeff6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f7f879573c5e491049d",
+    "url": "/BotFramework-WebChat/06.recomposing-ui/a.minimizable-web-chat/static/js/main.0b0b57a1.chunk.js"
+  },
+  {
+    "revision": "ce8431a2fd6e4195f489",
+    "url": "/BotFramework-WebChat/06.recomposing-ui/a.minimizable-web-chat/static/js/runtime-main.0e5c13f8.js"
+  },
+  {
+    "revision": "d6d7485af88eec33605ea6ff11ecbddf",
+    "url": "/BotFramework-WebChat/06.recomposing-ui/a.minimizable-web-chat/static/media/WebPage.d6d7485a.jpg"
+  }
+]);
